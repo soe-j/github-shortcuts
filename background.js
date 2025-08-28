5176f4af9e192782bcc9a68d38ccd4a5127419ba
soe-j <7115203+soe-j@users.noreply.github.com>
2025-08-28 14:57:36 +0900

    feat: added some logs

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
+const log = (...args) => console.log("[Github Shortcuts Background]", ...args);
+
 chrome.commands.onCommand.addListener(async (command) => {
+  log("received command", command);
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  log("active tab", tabs[0]);
   await chrome.tabs.sendMessage(tabs[0].id, { command });
 });
