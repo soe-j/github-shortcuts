82d4f2c65449afd14ee8f2fe0b024a1ddd1bee08
soe-j <7115203+soe-j@users.noreply.github.com>
2025-10-20 16:31:48 +0900

    for using on Arc browser, key bind must be set as global, then background.js gets enable for all pages

--- background.js
+++ background.js
@@ -4,5 +4,14 @@ chrome.commands.onCommand.addListener(async (command) => {
   log("received command", command);
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   log("active tab", tabs[0]);
+
+  if (
+    !tabs[0] ||
+    !/^https:\/\/github\.com\/[^\/]+\/[^\/]+\/pull\/\d+/.test(tabs[0].url)
+  ) {
+    log("no active github pull request tab found");
+    return;
+  }
+
   await chrome.tabs.sendMessage(tabs[0].id, { command });
 });
